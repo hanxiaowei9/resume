66f67fc3e09f13b93b9596205e6b77091326e759
hanwei <hanxiaowei92@163.com>
2017-08-26 00:39:44 +0800

    使用多种方法制作简历页面
    暂时先用vh属性或者fullpage.js做，但是slide属性无法左右旋转

---
+++ main.js
@@ -0,0 +1,12 @@
+$(function(){
+    $('#pages').fullpage();
+    setInterval(function(){
+        $.fn.fullpage.moveSlideRight();
+    }, 3000);
+    
+    setInterval(function(){
+        $.fn.fullpage.moveSlideRight();
+    }, 3000);
+    
+});
+
